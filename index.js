de5e46ce516f8a683c820016c8fca3a0985510a4
Captainfaisal13 <shaikhfaisal3412@gmail.com>
2022-03-30 23:15:24 +0530

    Updated navigation bar and fixed logo error

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+const menu = document.querySelector(".nav-wrap");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+
+menuBtn.addEventListener("click", function () {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+})
+
+cancelBtn.addEventListener("click", function () {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+});
+
+
 $("#nav").on('click', 'a', function (event) {
   if (this.innerText !== "About Us") {
     event.preventDefault();
     var o = $($(this).attr("href")).offset();
     var sT = o.top - $(".navbar").outerHeight(true);
     window.scrollTo(0, sT);
-  } else {
-
   }
 });
